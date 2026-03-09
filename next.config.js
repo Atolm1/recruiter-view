56379ed1ce5732792e6e2e7dfc23c55ec7afde37
Atolm1 <vortextempest@gmail.com>
2026-03-08 21:28:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *;" },
+        ],
+      },
+    ];
+  },
+};
 
+module.exports = nextConfig;
